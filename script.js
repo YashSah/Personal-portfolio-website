dde7f3298caf7997f00e9669b574ebcd1df2cc37
YashSah <yashsahyashu4752@gmail.com>
2023-08-23 15:34:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+
+circleMouseFollower();
